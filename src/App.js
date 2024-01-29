24674eaf053535d9cce5389d8b7586cc3623e92b
AlexandraPesneaud <alexandra.pesneaud@hotmail.com>
2024-01-29 10:37:10 +0100

    le premier composant

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Quote from './Quote/Quote.js'
+
 
 function App() {
   return (
-  <div className="App">
-  Quotes !!
-  </div>
+    <div className="App">
+      <h1>
+        Mes citations !
+      </h1>
+      <div className="App">
+        <Quote text="Elementary, my dear Watson" author="Sherlock Holmes"/>
+      </div>
+    </div>
   );
-  }  
+}
 
 export default App;
---
+++ src/Quote/Quote.js
@@ -0,0 +1,13 @@
+function Quote(props) {
+    return (
+        <>
+            <div>
+                Texte : {props.text} 
+            </div>
+            <div>
+                Auteur : {props.author}
+            </div>
+        </>);
+}
+
+export default Quote;
